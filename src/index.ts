9b1b0e05f69833c0acca7bef04dbaf914cf1129b
Oliver-Strange <preston.burton@gmail.com>
2020-06-29 07:30:28 -0700

    updates index

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,10 @@ export * from './errors/database-connection-error';
 export * from './errors/not-authorized-error';
 export * from './errors/not-found-error';
 export * from './errors/request-validation-error';
-
 export * from './middlewares/currentUser';
 export * from './middlewares/error-handler';
 export * from './middlewares/requireAuth';
 export * from './middlewares/validate-request';
-
 export * from './events/base-listener';
 export * from './events/base-publisher';
 export * from './events/subjects';
